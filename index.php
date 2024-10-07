7a8783de1a9423adedadaaa8f26f271db8b9aaa4
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-07 15:26:48 +0200

    added header

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong password generator</title>
+
+    <!-- bootstrap cdn 5.3.3 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1 class="text-center mt-5 mb-3">Strong Password Generator</h1>
+
+        <h2 class="text-center text-white mb-3">Genera un password sicura</h2>
+    </header>
+</body>
+
+</html>
